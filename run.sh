1d32407a9ffb88334a4772d0380b8ec874ba3302
VoidMercy <voidmercy@perfect.blue>
2024-02-18 19:39:06 -0800

    Change batch sizes

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 # Pretrain
 python3 multitask_classifier.py --use_gpu --task sst --f baseline_pretrain --lr 1e-3 --option pretrain --batch_size 64 $@
-python3 multitask_classifier.py --use_gpu --task sts --f baseline_pretrain --lr 1e-3 --option pretrain --batch_size 64 $@
+python3 multitask_classifier.py --use_gpu --task sts --f baseline_pretrain --lr 1e-3 --option pretrain --batch_size 32 $@
 python3 multitask_classifier.py --use_gpu --task lin --f baseline_pretrain --lr 1e-3 --option pretrain --batch_size 64 $@
-python3 multitask_classifier.py --use_gpu --task para --f baseline_pretrain --lr 1e-3 --option pretrain --batch_size 64 $@
+python3 multitask_classifier.py --use_gpu --task para --f baseline_pretrain --lr 1e-3 --option pretrain --batch_size 24 $@
 
 # Finetune
 python3 multitask_classifier.py --use_gpu --task sst --f baseline_finetune --lr 1e-5 --option finetune --batch_size 64 $@
-python3 multitask_classifier.py --use_gpu --task sts --f baseline_finetune --lr 1e-5 --option finetune --batch_size 64 $@
+python3 multitask_classifier.py --use_gpu --task sts --f baseline_finetune --lr 1e-5 --option finetune --batch_size 32 $@
 python3 multitask_classifier.py --use_gpu --task lin --f baseline_finetune --lr 1e-5 --option finetune --batch_size 64 $@
-python3 multitask_classifier.py --use_gpu --task para --f baseline_finetune --lr 1e-5 --option finetune --batch_size 64 $@
+python3 multitask_classifier.py --use_gpu --task para --f baseline_finetune --lr 1e-5 --option finetune --batch_size 24 $@
--- run_per_layer_finetune.sh
+++ run_per_layer_finetune.sh
@@ -2,6 +2,6 @@
 
 # Finetune
 python3 multitask_classifier.py --use_gpu --task sst --f per_layer_finetune --lr 1e-5 --option finetune --enable_per_layer_finetune --batch_size 64 $@
-python3 multitask_classifier.py --use_gpu --task sts --f per_layer_finetune --lr 1e-5 --option finetune --enable_per_layer_finetune --batch_size 64 $@
+python3 multitask_classifier.py --use_gpu --task sts --f per_layer_finetune --lr 1e-5 --option finetune --enable_per_layer_finetune --batch_size 32 $@
 python3 multitask_classifier.py --use_gpu --task lin --f per_layer_finetune --lr 1e-5 --option finetune --enable_per_layer_finetune --batch_size 64 $@
-python3 multitask_classifier.py --use_gpu --task para --f per_layer_finetune --lr 1e-5 --option finetune --enable_per_layer_finetune --batch_size 64 $@
+python3 multitask_classifier.py --use_gpu --task para --f per_layer_finetune --lr 1e-5 --option finetune --enable_per_layer_finetune --batch_size 24 $@
