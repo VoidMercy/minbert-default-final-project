0f1088ab2978b42fa587cba17bfcdfc74520a474
Alex Lin <alexlin0@rice11.stanford.edu>
2024-03-05 15:48:38 -0800

    Scripts

---
+++ scripts/b14_10.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=20G
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=1-20
+module load anaconda3
+module load cuda
+module load cudnn
+conda run --cwd /home/alexlin0/minbert-default-final-project -n pytorch python3 multitask_classifier.py --use_gpu --task sst --f b14_10 --lr 1e-5 --option finetune --enable_per_layer_finetune --batch_size 24 --epochs 10 --load_pretrain /farmshare/user_data/alexlin0/epoch_10_f-pretraining-sst.pt
